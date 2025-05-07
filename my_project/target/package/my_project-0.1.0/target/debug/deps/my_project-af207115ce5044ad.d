f6f165b7c05731456ffa1632c76e87b4aa8985b7
Daniel Perez-Rodriguez <112608773+Wemisiu@users.noreply.github.com>
2025-05-07 07:45:31 +0000

    Getting things Started

---
+++ my_project/target/package/my_project-0.1.0/target/debug/deps/my_project-af207115ce5044ad.d
@@ -0,0 +1,5 @@
+/workspaces/rustprogramming/my_project/target/debug/deps/my_project-af207115ce5044ad: src/main.rs
+
+/workspaces/rustprogramming/my_project/target/debug/deps/my_project-af207115ce5044ad.d: src/main.rs
+
+src/main.rs:
---
+++ my_project/target/package/my_project-0.1.0/target/debug/my_project.d
@@ -0,0 +1,1 @@
+/workspaces/rustprogramming/my_project/target/package/my_project-0.1.0/target/debug/my_project: /workspaces/rustprogramming/my_project/target/package/my_project-0.1.0/src/main.rs
