95936edd08665bfef6d72bd418c6734c2cd58205
Daniel Perez-Rodriguez <112608773+Wemisiu@users.noreply.github.com>
2025-05-09 06:31:07 +0000

    I got this!

--- project/src/main.rs
+++ project/src/main.rs
@@ -23,6 +23,15 @@ mod tests {
 
 fn main() {
 
+
+   
+        let mut vec: Vec<i32> = vec![1, 2, 3];
+        vec.push(4);
+        
+        println!("Vector: {:?}", vec);
+        println!("Vector length: {}", vec.len());
+    
+
     
     
 }
